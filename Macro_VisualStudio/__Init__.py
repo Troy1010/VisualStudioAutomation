21f315f9b74b7d38829030c9ec0181a06d1fe505
Unknown <drmcmurderface2@mail.com>
2018-05-28 03:02:15 -0500

    thrth

--- Macro_VisualStudio/__Init__.py
+++ Macro_VisualStudio/__Init__.py
@@ -21,14 +21,69 @@ def Register():
 def InstantiateDTE():
     return win32com.client.Dispatch("VisualStudio.DTE.15.0")
 
+def QuitDTE(vDTE):
+    vDTE.Solution.Close()
+    vDTE.Quit()
+
 #------Public
 
-def Proj_AddProps():
-    pass
+def AddProjObj(vDTE):
+    #---Open
+    sProjFile = os.path.abspath(sProjFile)
+    #---Filter
+    if not os.path.isfile(sProjFile):
+        raise Exception("sProjFile does not exist:"+sProjFile)
+    #---GetProjObj
+    vDTE = InstantiateDTE() #Do the DTEs self-destruct?
+    return vDTE.Solution.AddFromFile(sProjFile)
 
-def ProjItem_Filter(vProjectItem):
-    #vProjectItem.AddFilter()
-    pass
+def Proj_LinkToProps(sProjFile,sProps):
+    #---Open
+    sProjFile = os.path.abspath(sProjFile)
+    sProps = os.path.abspath(sProps)
+    #---Filter
+    if not os.path.isfile(sProjFile):
+        raise Exception("sProjFile does not exist:"+sProjFile)
+    if not os.path.isfile(sProps):
+        raise Exception("sProps does not exist:"+sProps)
+    #---LinkToProps
+    #-Get vProject
+    vDTE = InstantiateDTE() #Do the DTEs self-destruct?
+    vProject = vDTE.Solution.AddFromFile(sProjFile)
+    #-LinkToProps
+    #vProject.
+    #-Save
+    vProject.Save()
+
+#def ProjItem_Filter(sFilterName,vProjectItem,sFile):
+#    vProj = vProjectItem.ContainingProject
+#    for vFilter in vProj.Filters:
+#        vFilter.AddFile(sFile)
+
+#def ProjItem_Filter(sFile,vProjectItem):
+#    vProj = vProjectItem.ContainingProject
+#    for vFilter in vProj.Filters:
+#        vFilter.AddFile(sFile)
+
+#def ProjItem_Filter(sFilterName,vProjItem):
+#    vProj = vProjItem.ProjectItems.ContainingProject
+#    sProjItemName = vProjItem.Name
+#    vFile = vProj.Object.Files.Item(sProjItemName)
+#    vProj.Object.RemoveFile(vFile)
+#    vFilter = vProj.Object.AddFilter(sFilterName)
+#    vFilter.AddFile(sProjItemName)
+
+def AddFilterToProjItem(sFilterName,vProjItem):
+    sProjItemName = vProjItem.Name
+    vProj = vProjItem.ProjectItems.ContainingProject
+    vProj.Object.RemoveFile(vProjItem.Object)
+    vFilter = vProj.Object.AddFilter(sFilterName)
+    vFilter.AddFile(sProjItemName)
+
+def Proj_Filter2(sFile,sFilterName,vProj):
+    sFile = os.path.abspath(sFile)
+    vFilter = vProj.Object.AddFilter(sFilterName)
+    vFilter.AddFile(sFile)
 
 def Proj_AddFiles(sProjFile,cFilesToAdd):
     cReturning = []
@@ -36,6 +91,11 @@ def Proj_AddFiles(sProjFile,cFilesToAdd):
         vProjectItem = Proj_AddFile(sProjFile,vValue)
         cReturning.append(vProjectItem)
     return cReturning
+
+def Proj_AddFile2(vProj,sFileToAdd):
+    vProjectItem = vProj.ProjectItems.AddFromFile(sFileToAdd)
+#    vProj.Save()
+    return vProjectItem
 
 def Proj_AddFile(sProjFile,sFileToAdd):
     #---Open
@@ -51,7 +111,10 @@ def Proj_AddFile(sProjFile,sFileToAdd):
     vProject = vDTE.Solution.AddFromFile(sProjFile)
     vProjectItem = vProject.ProjectItems.AddFromFile(sFileToAdd)
     vProject.Save()
+    QuitDTE(vDTE)
     return vProjectItem
 
-def Filter(vProjectItem):
-    pass
+def Proj_Filter(sFilterName,vProject):
+    vFilter = vProject.Object.project.AddFilter(sFilterName)
+    vProject.Save()
+    return vFilter
--- Macro_VisualStudio/tests/Macro_VisualStudio_Test.py
+++ Macro_VisualStudio/tests/Macro_VisualStudio_Test.py
@@ -11,6 +11,8 @@ import Macro_VisualStudio as MVS
 import Macro_VisualStudio.Narrator
 import subprocess, win32com
 
+import win32com.client
+
 class Test_TM_CommonPy(unittest.TestCase):
     @classmethod
     def setUpClass(self):
@@ -30,19 +32,156 @@ class Test_TM_CommonPy(unittest.TestCase):
 
     #------Tests
 
+    #Working
+#    def test_Proj_AddFile(self):
+#        os.chdir('..')
+#        TMC.Copy('Examples_Backup','Examples_Proj_AddFile',bPreDelete=True)
+#        os.chdir('Examples_Proj_AddFile')
+#        MVS.Proj_AddFile("HelloWorld.vcxproj","HelloWorld2.cpp")
+
+    #Working
+#    def test_NarrateProj(self):
+#        vDTE = MVS.InstantiateDTE()
+#        vProj = vDTE.Solution.AddFromFile(os.path.abspath("HelloWorld.vcxproj")).object
+#        print(TMC.Narrator.Narrate(vProj,bIncludeProtected=True,bIncludePrivate=True))
+#        MVS.QuitDTE(vDTE)
+#        #self.assertTrue(False)
+
+    #Working
+#    def test_Proj_Filter(self):
+#        #---Open
+#        vDTE = MVS.InstantiateDTE()
+#        #---
+#        os.chdir('..')
+#        TMC.Copy('Examples_Backup','Examples_Proj_Filter',bPreDelete=True)
+#        os.chdir('Examples_Proj_Filter')
+#        #---
+#        vProject = vDTE.Solution.AddFromFile(os.path.abspath("HelloWorld.vcxproj"))
+#        MVS.Proj_Filter("FilterName1",vProject)
+#        #---Close
+#        MVS.QuitDTE(vDTE)
+#        #self.assertTrue(False)
+
+    #Working
+#    def test_NarrateProj(self):
+#        vDTE = MVS.InstantiateDTE()
+#        vProj = vDTE.Solution.AddFromFile(os.path.abspath("HelloWorld.vcxproj"))
+#        vProj2 = vProj.Object.project
+#        print(TMC.Narrator.Narrate(vProj2,bIncludeProtected=True,bIncludePrivate=True))
+#        MVS.QuitDTE(vDTE)
+#        self.assertTrue(False)
+
+    #Working
+#    def test_ProjItem_Filter(self):
+#        #---Open
+#        vDTE = MVS.InstantiateDTE()
+#        #---
+#        os.chdir('..')
+#        TMC.Copy('Examples_Backup','Examples_ProjItem_Filter',bPreDelete=True)
+#        os.chdir('Examples_ProjItem_Filter')
+#        #--- Get vProj
+#        vProj = vDTE.Solution.AddFromFile(os.path.abspath("HelloWorld.vcxproj"))
+#        #--- Add File
+#        vProjectItem = vProj.ProjectItems.AddFromFile(os.path.abspath("HelloWorld2.cpp"))
+#        #--- Add Filter
+#        vFilter = vProj.Object.AddFilter("Filter1")
+#        #--- Add File to Filter
+#        vFilter.AddFile(os.path.abspath("HelloWorld3.cpp"))
+#        #--- Save
+#        vProj.Save()
+#        #--- Close
+#        MVS.QuitDTE(vDTE)
+
+    #Working
+    def test_ProjItem_Filter(self):
+        #---Open
+        vDTE = MVS.InstantiateDTE()
+        #---
+        os.chdir('..')
+        TMC.Copy('Examples_Backup','Examples_ProjItem_Filter',bPreDelete=True)
+        os.chdir('Examples_ProjItem_Filter')
+        #---Get vProj
+        vProj = vDTE.Solution.AddFromFile(os.path.abspath("HelloWorld.vcxproj"))
+        vProjItem = vProj.ProjectItems.AddFromFile("HelloWorld3.cpp")
+        #    sFilterName    vProjItem
+        MVS.AddFilterToProjItem("Filter7",vProjItem)
+        #---Save
+        vProj.Save()
+        #---Close
+        MVS.QuitDTE(vDTE)
+
+
+
+
+
+
+
+
+#--- Add File
+#        vProjectItem = vProj.ProjectItems.AddFromFile(os.path.abspath("HelloWorld2.cpp"))
+#        vProjectItems = vProj.ProjectItems
+
+#        vProj = vDTE.Solution.AddFromFile(os.path.abspath("HelloWorld.vcxproj"))
+#        #--- Add File
+#        sFileToAdd = "HelloWorld2.cpp"
+#        sFileToAdd = os.path.abspath(sFileToAdd)
+#        if not os.path.isfile(sFileToAdd):
+#            raise Exception("sFileToAdd does not exist:"+sFileToAdd)
+#        vProjectItem = vProj.ProjectItems.AddFromFile(sFileToAdd)
+#        vFile = vProj.ProjectItems.AddFromFile(os.path.abspath("HelloWorld2.cpp"))
+#        vProjectItem = vProj.ProjectItems.AddFromFile(os.path.abspath("HelloWorld2.cpp"))
+#        vFile = vProj.ProjectItems.AddFromFile("HelloWorld2.cpp")
+        #--- Add Filter
+#        vFilter = vProj.Object.project.AddFilter("FilterName1")
+        #--- Add File to Filter
+#        vFilter.AddFile(os.path.abspath("HelloWorld2.cpp"))
+        #--- Save
+#        vProj.Object.project.Save()
+        #---
+#        for vFilter in vProj.Filters:
+#            vFilter.AddFile(vFile)
+#            break
+#        vProjItems = vProj.ProjectItems
+#        for vProjItem in vProjItems:
+#            MVS.ProjItem_Filter(vProjItem.Name,vProjItem)
+#            vProj = vProjectItem.ContainingProject
+#            for vFilter in vProj.Filters:
+#                vFilter.AddFile(sFile)
+#            break
+        #---Close
+#        MVS.QuitDTE(vDTE)
+#        self.assertTrue(False)
+
+#    def test_Proj_LinkToProps(self):
+#        os.chdir('..')
+#        TMC.Copy('Examples_Backup','Examples_Proj_LinkToProps',bPreDelete=True)
+#        os.chdir('Examples_Proj_LinkToProps')
+#        MVS.Proj_LinkToProps("HelloWorld.vcxproj","conanbuildinfo.props")
+
+#    def test_NarrateProj2(self):
+#        vDTE = MVS.InstantiateDTE()
+#        vProj = vDTE.Solution.AddFromFile(os.path.abspath("HelloWorld.vcxproj"))
+#        print(TMC.Narrator.Narrate(vProj,bIncludeProtected=False))
+#        MVS.QuitDTE(vDTE)
+#        #self.assertTrue(False)
+
+#    def test_NarrateProj2(self):
+#        print(TMC.Narrator.Narrate(MVS.GetProjObj("HelloWorld.vcxproj"),bIncludeProtected=False))
+#        self.assertTrue(False)
+
 #    def test_Proj_AddFile(self):
 #        MVS.Proj_AddFile("HelloWorld.vcxproj","HelloWorld2.cpp")
 
-    def test_2(self):
-        vDTE = win32com.client.Dispatch("VisualStudio.DTE.15.0")
-        vProject = vDTE.Solution.AddFromFile(os.path.abspath('HelloWorld.vcxproj'))
-        print(MVS.Narrator.NarrateProject(vProject))
-        vProjectItems = vProject.Projectitems
-        #print(MVS.Narrator.NarrateProject(vProject))
-        #print("Type:"+str(type(vProjectItems)))
-        #print("zz:"+str(vProjectItems))
-        #print("vProject.Projectitems.Count:"+str(vProject.Projectitems.Count))
-        self.assertTrue(False)
+#    def test_2(self):
+#        vDTE = win32com.client.Dispatch("VisualStudio.DTE.15.0")
+#        vProject = vDTE.Solution.AddFromFile(os.path.abspath('HelloWorld.vcxproj'))
+#        print(MVS.Narrator.NarrateProject(vProject))
+#        vProjectItems = vProject.Projectitems
+#        #print(MVS.Narrator.NarrateProject(vProject))
+#        #print("Type:"+str(type(vProjectItems)))
+#        #print("zz:"+str(vProjectItems))
+#        #print("vProject.Projectitems.Count:"+str(vProject.Projectitems.Count))
+#        self.assertTrue(False)
 
 #    def test_1(self):
 #        #---Run HookMultiThreadMessageHandler
