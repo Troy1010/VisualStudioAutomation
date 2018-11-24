2fce71e2078bde0dcc2d4849c18fc8e69b672091
Unknown <drmcmurderface2@mail.com>
2018-11-24 01:52:55 -0600

    __init__-simpler imports

--- VisualStudioAutomation/__Init__.py
+++ VisualStudioAutomation/__Init__.py
@@ -6,21 +6,19 @@ bRetryGenericCOMErrors = True
 bRetryTypeErrors = True
 ##endregion
 
-import TM_CommonPy as TM
-import VisualStudioAutomation as VS
-from VisualStudioAutomation.Misc import IsRetryableException
-from VisualStudioAutomation.Misc import RemoveProjectFromSlnFile
-from VisualStudioAutomation.Misc import Find
-from VisualStudioAutomation.Misc import FindByPath
-from VisualStudioAutomation.Misc import CorruptSolution
-from VisualStudioAutomation.Misc import MakeProjectPathsRelativeInSlnFile
-from VisualStudioAutomation.DTEWrapper import DTEWrapper
-from VisualStudioAutomation.ProjWrapper import ProjWrapper
-from VisualStudioAutomation.SlnWrapper import SlnWrapper
-from VisualStudioAutomation.ConvenienceEtree import IntegrateProps
-from VisualStudioAutomation.ConvenienceEtree import IntegrateProps_Undo
-from VisualStudioAutomation.ConvenienceEtree import SetTMDefaultVSSettings
-from VisualStudioAutomation.ConvenienceEtree import SetIncludeDir
-from VisualStudioAutomation.ConvenienceEtree import GetProjectGUID
+from .Misc import IsRetryableException
+from .Misc import RemoveProjectFromSlnFile
+from .Misc import Find
+from .Misc import FindByPath
+from .Misc import CorruptSolution
+from .Misc import MakeProjectPathsRelativeInSlnFile
+from .DTEWrapper import DTEWrapper
+from .ProjWrapper import ProjWrapper
+from .SlnWrapper import SlnWrapper
+from .ConvenienceEtree import IntegrateProps
+from .ConvenienceEtree import IntegrateProps_Undo
+from .ConvenienceEtree import SetTMDefaultVSSettings
+from .ConvenienceEtree import SetIncludeDir
+from .ConvenienceEtree import GetProjectGUID
 
 __version__ = '0.20.0'
