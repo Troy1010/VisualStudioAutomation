90da40183a55feafa9856ed77a32988a8fa2039c
Unknown <drmcmurderface2@mail.com>
2018-11-30 01:51:03 -0600

    Getting LoggingHeaderDecorator from TM_CommonPy

--- VisualStudioAutomation/_tests/_Logger.py
+++ VisualStudioAutomation/_tests/_Logger.py
@@ -8,25 +8,9 @@ vFileHandlerThreshold = logging.DEBUG
 ##endregion
 import TM_CommonPy as TM
 
-class HeaderDecorator:
-    """Decorator to add a header"""
-    bFirstCall=True
-    sLastFnName=""
-    def __init__(self,method):
-        self.method=method
-    def __call__(self,*args,**kwargs):
-        if __class__.sLastFnName != TM.FnName(1):
-            __class__.sLastFnName = TM.FnName(1)
-            if __class__.bFirstCall:
-                __class__.bFirstCall=False
-                self.method("-------"+__class__.sLastFnName)
-            else:
-                self.method("\n\n-------"+__class__.sLastFnName)
-        self.method(*args, **kwargs)
-
 VSLog_LogTests = logging.getLogger(__name__)
-VSLog_LogTests.info = HeaderDecorator(VSLog_LogTests.info)
-VSLog_LogTests.debug = HeaderDecorator(VSLog_LogTests.debug)
+VSLog_LogTests.info = TM.LoggingHeaderDecorator(VSLog_LogTests.info)
+VSLog_LogTests.debug = TM.LoggingHeaderDecorator(VSLog_LogTests.debug)
 VSLog_LogTests.setLevel(vMasterThreshold)
 vFormatter = logging.Formatter('%(message)s')
 #---FileHandler
