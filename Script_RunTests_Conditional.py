17e71015e2eebf7f19066e93165e6361f1fbed37
Troy1010 <drmcmurderface2@mail.com>
2018-12-03 06:21:42 -0600

    .gitignore

--- Script_RunTests_Conditional.py
+++ Script_RunTests_Conditional.py
@@ -1,3 +1,3 @@
 import TM_CommonPy as TM
-sEval = '(count < 3 and VisualStudioAutomation_Tests) or (count < 1 and VSA_ConvenienceEtree_Tests)'
-TM.devtools.RunTests(sTestPath="VisualStudioAutomation/_tests", sEval=sEval, bPause=False)
+TM.devtools.RunTests(sTestPath="VisualStudioAutomation/_tests", bPause=False, iVerbosity=3,
+    sEval='(count < 3 and VisualStudioAutomation_Tests) or (count < 1 and VSA_ConvenienceEtree_Tests)')
